817260a5fe655e847f919636deb7fa64c22893b3
shobhitsrivastava2023 <shobhit.229302512@muj.manipal.edu>
2024-09-03 14:20:23 +0530

    added search functionality

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -2,20 +2,26 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Loader2, Search } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { startTransition, useRef, useState, useTransition } from "react";
 
-const SearchBar = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [isSearching, setIsSearching] = useTransition();
-  const router = useRouter();
-  const [query, setQuery] = useState<string>("");
 
-  const search = () => {
-    startTransition(() => {
-      router.push(`/search?query=${query}`);
-    });
-  };
+export const SearchBar = () => {
+  const searchParams = useSearchParams(); 
+  const pathname = usePathname();
+  const {replace} = useRouter(); 
+
+ 
+
+  function handleSearch(term: string) { 
+    const params = new URLSearchParams(searchParams); 
+    if(term){ 
+      params.set('query', term)
+    }else{
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
 
   return (
     <div className="flex justify-center items-center mt-16 mb-5">
@@ -23,31 +29,12 @@ const SearchBar = () => {
         <div className="relative w-[700px] h-[55px] z-10">
           <div>
             <Input
-              disabled={isSearching}
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              ref={inputRef}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  search();
-                }
-                if (e.key === "Escape") {
-                  inputRef?.current?.blur();
-                }
-              }}
-              placeholder="Search Your College"
-              className="rounded-full"
+            placeholder="Search Your College"
+            className="rounded-full"
+            onChange={(e) => {handleSearch(e.target.value)}}
+            defaultValue={searchParams.get('query')?.toString()}
             />
-            <Button
-              onClick={search}
-              disabled={isSearching}
-              variant={"tertiary"}
-              className="absolute right-0 inset-y-0 rounded-l-none bg-white rounded-full"
-            >
-              {isSearching ? <Loader2 className="h-6 w-6 animate-spin"/> : 
-              <Search className="text-[#1C1B1F] h-6 w-6" />
-}
-            </Button>
+            
 
             
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,59 @@
 import CollegesHome from "./components/CollegesHome";
 import Nav from "./components/Nav";
 import SearchBar from "./components/SearchBar";
+import prisma from "@/db/prisma";
 
-export default function Home() {
+async function fetchCollegeNames(query: string) {
+  const names = await prisma.collegesName.findMany({
+    where: {
+      name: {
+        contains: query,
+        mode: 'insensitive',
+      },
+    },
+    select: { name: true },
+    take: 3,
+  });
+  return names;
+}
+
+export default async function Home({ searchParams }: {
+  searchParams?: {
+    query?: string,
+    page?: string,
+  },
+}) {
+  const query = searchParams?.query || '';
+  const collegeNames = await fetchCollegeNames(query);
+
   return (
-    <div className="flex flex-col space-y-5">
-      <SearchBar/>
-      <CollegesHome/>
-    </div>
+    <>
+      <div className="flex flex-col space-y-5">
+        <SearchBar />
+        {query && (
+          <div className="p-4 m-5">
+            <table className="min-w-full divide-y divide-gray-200 mt-4">
+              <thead>
+                <tr>
+                  <th scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    College Name
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {collegeNames.map((collegeName, index) => (
+                  <tr key={index}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                      {collegeName.name}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+      <CollegesHome />
+    </>
   );
 }
